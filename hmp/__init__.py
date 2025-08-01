fc85038685217bcaedc9ebbd8979c5a80b0fcaea
Raoul Schram <r.d.schram@uu.nl>
2025-08-01 14:19:54 +0200

    Allow __version__ to be imported directly from hmp

--- hmp/__init__.py
+++ hmp/__init__.py
@@ -1,5 +1,14 @@
 """Software for fitting HMP on EEG/MEG data."""
 
+from importlib.metadata import version, PackageNotFoundError
+
 from . import loocv, models, utils, visu, io, preprocessing, patterns, distributions
 
-__all__ = ["models", "simulations", "utils", "visu", "io", "preprocessing", "patterns", "distributions" ,"mcca", "loocv"]
+try:
+    __version__ = version("hmp")
+except PackageNotFoundError:
+    __version__ = "unknown"
+
+
+__all__ = ["models", "simulations", "utils", "visu", "io", "preprocessing", "patterns",
+           "distributions" ,"mcca", "loocv", "__version__"]
