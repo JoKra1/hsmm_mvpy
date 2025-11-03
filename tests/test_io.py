a6ce9447f48e33418fee12bc0946af09b09d3529
GWeindel <gabriel.weindel@proton.me>
2025-11-03 09:30:42 +0100

    Work on IO
    - empty file list means bids
    - removed all functions related to preprocessing
    - no mentions of RT anymore
    - Added coherent tests in test_io

--- tests/test_io.py
+++ tests/test_io.py
@@ -1,6 +1,13 @@
 from pathlib import Path
 
 import numpy as np
+import mne_bids
+import shutil
+import platform
+import os
+import requests
+from zipfile import ZipFile
+
 
 from hmp import io
 from hmp import simulations
@@ -17,6 +24,7 @@ DATA_DIR_A = DATA_DIR / "dataset_a"
 DATA_DIR_B = DATA_DIR / "dataset_b"
 
 def init_data():
+    # Also tests data_format 'raw'
     """ Initialize all data and model related info."""
     sfreq = 100
     n_events = 3
@@ -37,6 +45,65 @@ def init_data():
     positions = simulations.positions()
     return event_b, event_a, epoch_data, positions, sfreq, n_events
 
+def test_epochs():
+    # Declaring path where the EEG data will be stored
+    epoch_data_path = os.path.join('sample_data', 'eeg')
+    os.makedirs(epoch_data_path, exist_ok=True)
+    
+    # URLs of the first two participants
+    file_urls = [
+        "https://osf.io/download/67cffa85f67af67e7a92f0a6/",
+    ]
+    
+    # Download and save each file if not already in folder
+    for i, url in enumerate(file_urls, start=1):
+        file_path = os.path.join(epoch_data_path, f'participant{i}_epo.fif')
+        if not os.path.exists(file_path):
+            response = requests.get(url)
+            with open(file_path, 'wb') as f:
+                f.write(response.content)
+    sfreq = 100 #at what sampling rate we want the data, downsampling to 100Hz is computationally less intensive for hmp instances
+    
+    # Recovering individual files and participant names
+    subj_files = [os.path.join(epoch_data_path, f) for f in os.listdir(epoch_data_path) if f.endswith('.fif')]  # Create a list of files with full paths
+    subj_names = [os.path.splitext(f)[0] for f in os.listdir(epoch_data_path) if f.endswith('.fif')]  # Extract subject names based on file names
+    
+    # Then we read the data (see more in Tutorial 1)
+    epoch_data = io.read_mne_data(subj_files, sfreq=sfreq, data_format='epochs',
+                            verbose=False, subj_name=subj_names)#Turning verbose off for the documentation but it is recommended to leave it on as some output from MNE might be useful
+
+def test_bids():
+    # Testing on small bids dataset (1.8 GB)
+    erp_core_url = "https://osf.io/download/3zk6n/"
+    zip_path = "ERP_CORE_P3.zip"
+    extract_dir = "./"
+    
+    if not os.path.exists(zip_path):
+        print("Downloading ERP CORE dataset...")
+        r = requests.get(erp_core_url, stream=True)
+        with open(zip_path, "wb") as f:
+            for chunk in r.iter_content(chunk_size=8192):
+                f.write(chunk)
+    print("Extracting ERP CORE dataset...")
+    with ZipFile(zip_path, "r") as zip_ref:
+        zip_ref.extractall(extract_dir)
+    
+    sfreq = 250 
+    tmin, tmax = -.2, .8
+    epoch_data = io.read_mne_data([], 
+                                      data_format='bids',
+                                      tmin=tmin, tmax=tmax, 
+                                      sfreq=sfreq,
+                                      bids_parameters={
+                                          'bids_root': 'ERP_CORE',
+                                          'task': 'P3',
+                                          'datatype': 'eeg',
+                                          'session': 'P3'
+                                      },
+                                      reference='average',
+                                      verbose=False
+                                      )
+    
 
 def test_save_dat():
     event_b, event_a, epoch_data, positions, sfreq, n_events = init_data()
