7142d6a90628e5faa12dd98f3a2c52f984e9a71f
GWeindel <gabriel.weindel@proton.me>
2025-11-11 16:30:26 +0100

    Naming convention of modules

--- hmp/transformers/__init__.py
+++ hmp/transformers/__init__.py
@@ -1,7 +1,7 @@
 """Different methods for transforming E/MEG data to HMP ready data."""
 
-from hmp.transformers.ProjPCA import ProjPCA
-from hmp.transformers.ProjIdentity import ProjIdentity
-from hmp.transformers.ProjCustom import ProjCustom
+from hmp.transformers.pca import ProjPCA
+from hmp.transformers.identity import ProjIdentity
+from hmp.transformers.custom import ProjCustom
 
 __all__ = ["ProjPCA", "ProjIdentity", "ProjCustom"]
