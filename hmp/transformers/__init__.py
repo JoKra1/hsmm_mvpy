563e72965480b696a0b117a6b5167ff4c663b629
GWeindel <gabriel.weindel@proton.me>
2025-11-14 09:46:12 +0100

    Updated docs

--- hmp/transformers/__init__.py
+++ hmp/transformers/__init__.py
@@ -1,7 +1,8 @@
 """Different methods for transforming E/MEG data to HMP ready data."""
 
+from hmp.transformers.base import BaseTransformer
 from hmp.transformers.custom import ProjCustom
 from hmp.transformers.identity import ProjIdentity
 from hmp.transformers.pca import ProjPCA
 
-__all__ = ["ProjPCA", "ProjIdentity", "ProjCustom"]
+__all__ = ["BaseTransformer", "ProjPCA", "ProjIdentity", "ProjCustom"]
