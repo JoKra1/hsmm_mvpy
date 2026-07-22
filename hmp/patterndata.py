49249bfe0cdf207322e6d8cc8e5126e15ce8dcd6
GWeindel <gabriel.weindel@proton.me>
2026-07-22 09:10:29 +0200

    Adjust width of the HalfSine if it is not mathing target sfreq

--- hmp/patterndata.py
+++ hmp/patterndata.py
@@ -76,11 +76,9 @@ class PatternData:
         # Crosscorrelating data and pattern
         if pattern is None:
             pattern = HalfSine()
-        
-        template = pattern.template
 
         # Downsample and normalize the template 
-        template = _norm_template(data.sfreq, template)
+        template = _norm_template(data.sfreq, pattern)
 
         # Equation 1 in 2024 paper
         cross_corr, durations = cross_correlation(data.values, template,
@@ -106,7 +104,22 @@ class PatternData:
                     cross_corr=cross_corr, pattern=pattern, template=template,
                     sfreq=data.sfreq)
 
-def _norm_template(sfreq, template):
+def _adjust_hs_to_freq(sfreq, width):
+    n_samples = int(np.round((width/1000.0) * sfreq))
+    new_width = n_samples / sfreq * 1000
+    pattern = HalfSine(width=new_width)
+    return pattern
+    
+def _norm_template(sfreq, pattern):
+    if isinstance(pattern, HalfSine):
+        ori_width = len(pattern.template)
+        # Adjust target width if sfreq does not match initial created pattern
+        if not np.isclose((ori_width/1000.) * sfreq,
+                          round((ori_width/1000.) * sfreq)):
+            pattern = _adjust_hs_to_freq(sfreq, ori_width)
+            warn("Requested event duration and sampling frequency do not match, "
+                 f"adapting pattern to {np.round(pattern.width,2)} instead of {ori_width} ms")
+    template = pattern.template
     tstep = int(np.rint(1000/sfreq))
     template = template[tstep // 2 :: tstep]
     template = template / np.sum(template**2)
