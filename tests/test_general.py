2c837c2c4c6267507b252654eedf3011d831ead7
GWeindel <gabriel.weindel@proton.me>
2026-07-22 22:41:36 +0200

    adjusted tests

--- tests/test_general.py
+++ tests/test_general.py
@@ -16,16 +16,18 @@ n_events = len(names)-1 # Last is simulated resp
 # Remove default baseline corr
 epoching=dict(baseline=None)
 
-llks = {(100,50):-3.588911,
-        (200,25):-3.588911,
-        (256, 30):-4.5260143,
+llks = {(100, 50):-3.588911,
+        (100, 30):-2.5232670,
+        (200, 25):-3.588911,
+        (256, 35.5):-4.7588858,
         (1000,70):-8.8224058,
         }
 
 @pytest.mark.parametrize("sfreq,width",
                          [(100, 50),
+                          (100, 30),
                           (200, 25),
-                          (256, 30),#Wrong width
+                          (256, 26), # Cannot fit, should give 2 events max
                           (1000, 70),])
 def test_basic_operations(sfreq, width):
     """Simulate noiseless trial with minimum duration possible
@@ -33,8 +35,6 @@ def test_basic_operations(sfreq, width):
     n_trials = 1
     cpus=1
     gen_width = width
-    if (sfreq, width) == (256, 30): # Generate the true width
-        gen_width = 31.25
     # Minimum possible time with provided halfsine and 3 events
     times_a = np.array([[1000/sfreq, gen_width, gen_width, 1000/sfreq]], dtype='float64')
     sources = []
@@ -75,14 +75,17 @@ def test_basic_operations(sfreq, width):
     model = hmp.models.EventModel(n_events=3, pattern=pattern)
     llk, estimates = model.fit_transform(trial_data)
     test_topos = hmp.utils.event_channels(epoch_data, estimates, mean=True)
-    hmp.visu.plot_topo_timecourse(epoch_data, estimates,  info, as_time=False,)
-    assert (np.isclose(times_a, hmp.utils.event_times(estimates, duration=True, add_rt=True, as_time=True), atol=.01)).all(),\
-        'Incorrect timing extraction'
-    assert np.all(trial_data.cross_corr[peaks] == trial_data.cross_corr[peaks][0]),\
-        'Unexpected peak value in crosscorrelation'
-    assert np.isclose(llk, llks[(sfreq, width)], atol=1e-6), \
-        f'Incorrect likelihood from model, expected {llks[(sfreq, width)]} got {llk}'
-    assert (test_topos == test_topos.isel(event=0)).all(),\
-        'Incorrect topo calculation from estimates'
-    assert model._compute_max_events(trial_data, location=width) == 3, \
+    if (sfreq, width) == (256, 26):
+         assert model._compute_max_events(trial_data, location=width) == 2, \
         'Incorrect max event calculation'
+    else:
+        assert (np.isclose(times_a, hmp.utils.event_times(estimates, duration=True, add_rt=True, as_time=True), atol=.01)).all(),\
+            'Incorrect timing extraction'
+        assert np.all(trial_data.cross_corr[peaks] == trial_data.cross_corr[peaks][0]),\
+            'Unexpected peak value in crosscorrelation'
+        assert np.isclose(llk, llks[(sfreq, width)], atol=1e-6), \
+            f'Incorrect likelihood from model, expected {llks[(sfreq, width)]} got {llk}'
+        assert (test_topos == test_topos.isel(event=0)).all(),\
+            'Incorrect topo calculation from estimates'
+        assert model._compute_max_events(trial_data, location=width) == 3, \
+            'Incorrect max event calculation'
