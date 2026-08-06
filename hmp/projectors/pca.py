abd2756200662adda02285773ec7e480b8d2ba90
Rick den Otter <r.denotter@uu.nl>
2026-08-06 14:54:19 +0200

    Got rid of expensive .sel operation in trial loop of PCA covariance computation

--- hmp/projectors/pca.py
+++ hmp/projectors/pca.py
@@ -121,21 +121,21 @@ class PCA(Projector):
     @staticmethod
     def _compute_covariance(data):
         """Compute covariance of data trial by trial."""
-        vcov_mat = np.zeros((data.sizes["channel"], data.sizes["channel"]), dtype=np.float64)
-        # Iteratively for memory efficiency
+        arr = data.values  # (trial, channel, sample)
+        n_ch = arr.shape[1]
+        vcov_mat = np.zeros((n_ch, n_ch), dtype=np.float64)
         count = 0
-        for i in data.trial:
-            x_i = np.squeeze(data.sel(trial=i).values)
+        for x_i in arr:
             x_i = x_i[:, ~np.isnan(x_i[0, :])]
             if x_i.shape[1] > x_i.shape[0]:
                 count += 1
-                cov_i = (x_i @ x_i.T) / (x_i.shape[1]-1)
+                cov_i = (x_i @ x_i.T) / (x_i.shape[1] - 1)
                 # Regularization using MNE python's default
                 sigma = np.mean(np.diag(cov_i))
-                cov_i.flat[:: len(cov_i) + 1] += 0.1 * sigma
+                cov_i.flat[:: n_ch + 1] += 0.1 * sigma
                 vcov_mat += cov_i
-        if count < len(data.trial)/10:
+        if count < len(arr) / 10:
             warn(f"Less than 10% of the trials used to compute covariance for"
                  f"{np.unique(data.recording.values)}. Covariance matrix might be unreliable")
-        return vcov_mat/count
+        return vcov_mat / count
 
